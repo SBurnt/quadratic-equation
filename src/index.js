ccfd63cab9fc45631810cb0034af831bb72694da
SBurnt <zip2004zip@gmail.com>
2018-09-20 15:24:58 +0300

    task quadratic-equation first

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+
+  // ax2 + bx + с = 0
+  // d = b2 - 4ас
+  // if d>0 x1,2 = -b +- sqrt d / 2a
+  // if d=0 x = -b / 2a
+  // if d<0 x = NaN
+
+  // function solveEquation(equation) {
+
+  let arr = equation.split(' ');
+  let a = arr[0];
+  let b = (arr[3] + arr[4]);
+  let c = (arr[7] + arr[8]);
+
+  d = b * b - 4 * a * c;
+  // console.log("d", d);
+
+  var solutions = [];
+  solutions.push(Math.round((-b + Math.sqrt(d)) / (2 * a)));
+  solutions.push(Math.round((-b - Math.sqrt(d)) / (2 * a)));
+  // console.log(solutions);
+
+  return solutions.sort((a, b) => a - b);
 }
+
+// solveEquation('2 * x^2 - 10 * x + 12');
